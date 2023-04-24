ffc4f124450b3bd2c29f00c750b5aea1ac18372d
Amar Singh <t_amar.s@tatadigital.com>
2023-04-24 12:20:30 +0530

    Initial Commit

---
+++ TCG mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCG mobile
+//
+//  Created by Amar Kumar Singh on 24/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCG mobile/TCG_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCG_mobileApp.swift
+//  TCG mobile
+//
+//  Created by Amar Kumar Singh on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TCG_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
